e3a699fc2f8574ec76342d2978e32a60f3f825c6
diego7963 <elshrab90.ma@gmail.com>
2018-06-05 17:29:37 +0200

    api

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -9,7 +9,8 @@ class MoviesList extends Component {
     }
     async componentDidMount(){
         try{
-           const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=6869435511daf27f51f18e8e45518285&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+            //put yout api key instead of dots
+           const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=...&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
            const movies = await res.json();
            this.setState({
                movies:movies.results,
